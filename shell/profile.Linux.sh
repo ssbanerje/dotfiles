664db59488644da72ae1b2637d92acf4b8d598fd
Subho Banerjee <subs.zero@gmail.com>
2013-02-21 17:23:30 +0530

    pbcopy/paste in linux
    pbcopy/paste in linux

--- shell/profile.Linux.sh
+++ shell/profile.Linux.sh
@@ -1,3 +1,7 @@
+#Use pbcopy and pbpaste on Linux systems
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xsel --clipboard --output'
+
 #Matlab on linux
 export MATLAB=/usr/local/MATLAB/R2012b
 export PATH=$PATH:$MATLAB/bin
