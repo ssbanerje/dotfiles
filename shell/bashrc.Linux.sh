e040af9ab0b1d85b1eb450eaa9220cf993e26676
Subho Banerjee <subs.zero@gmail.com>
2013-09-27 12:11:24 -0500

    submodule update

--- shell/bashrc.Linux.sh
+++ shell/bashrc.Linux.sh
@@ -1,5 +1,9 @@
 # Install bash completion
-if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
-  . /etc/bash_completion
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
 fi
 
