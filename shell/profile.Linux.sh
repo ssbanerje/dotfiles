6afaa57380a3a45e08b88d15454919265a8a3589
Subho Banerjee <subs.zero@gmail.com>
2015-04-14 20:30:09 -0500

    Changes for linux

--- shell/profile.Linux.sh
+++ shell/profile.Linux.sh
@@ -1,15 +1,17 @@
-#Use pbcopy and pbpaste on Linux systems
+# Use pbcopy and pbpaste on Linux systems
 alias pbcopy='xsel --clipboard --input'
 alias pbpaste='xsel --clipboard --output'
 
 # Set PATH to include non-standard locations
 export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin:$HOME/.powerline/scripts
 
-#Set for CUDA
+# Set for CUDA
 CUDA=/usr/local/cuda
 export PATH=$PATH:$CUDA/bin
 
-#Matlab on linux
+# Matlab on linux
 export MATLAB=/usr/local/MATLAB/R2013b
 export PATH=$PATH:$MATLAB/bin
 
+# Source Intel compilers
+source /opt/intel/parallel_studio_xe_2015/psxevars.sh
