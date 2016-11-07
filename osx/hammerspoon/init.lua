d4c68e46105ffd4a89c14858c9abbe433ca24401
Subho Banerjee <ssbanerje@gmail.com>
2016-11-06 19:34:01 -0600

    Changing hammerspoon config

--- osx/hammerspoon/init.lua
+++ osx/hammerspoon/init.lua
@@ -1,124 +1,122 @@
 require "hs.wifi"
-require "hs.caffeinate"
+require "hs.window"
+require "hs.pathwatcher"
 
--- Set grid size.
-hs.grid.GRIDWIDTH  = 2
-hs.grid.GRIDHEIGHT = 2
+-- Grid settings
+hs.grid.GRIDWIDTH  = 3
+hs.grid.GRIDHEIGHT = 3
 hs.grid.MARGINX    = 0
 hs.grid.MARGINY    = 0
+hs.hotkey.bind("cmd", "G", hs.grid.show)
+
 -- Set window animation off. It's much smoother.
 hs.window.animationDuration = 0
 
 -- hot key
-hs.hotkey.bind("cmd", "J", function()
-                 hs.hints.windowHints(hs.window.visibleWindows(),
-                                      nil,
-                                      false
-                 )
-                 end
-)
+hs.hotkey.bind({"cmd", "ctrl"}, "J", function()
+  hs.hints.windowHints(hs.window.visibleWindows(), nil, false)
+end)
 
-hs.hotkey.bind("cmd", "G", hs.grid.show)
-hs.hotkey.bind("cmd", ";", hs.grid.maximizeWindow)
+hs.hotkey.bind("cmd", "M", hs.grid.maximizeWindow)
 
+-- Move windows
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Y", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  f.x = f.x - 10
+  f.y = f.y - 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "K", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  f.y = f.y - 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  f.x = f.x + 10
+  f.y = f.y - 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  f.x = f.x - 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  f.x = f.x + 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "N", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  f.x = f.x - 10
+  f.y = f.y + 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "J", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  f.y = f.y + 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  f.x = f.x + 10
+  f.y = f.y + 10
+  win:setFrame(f)
+end)
 
 -- reload config
-hs.hotkey.bind({"cmd", "shift", "ctrl"}, "R", function()
-      hs.reload()
-      hs.alert.show("Config reloaded!")
-end)
+function reloadConfig(files)
+  doReload = false
+  for _,file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      doReload = true
+    end
+  end
+  if doReload then
+    hs.reload()
+  end
+end
+local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Hammerspoon config loaded")
 
 -- Home
 hs.hotkey.bind({"cmd", "shift", "ctrl"}, "H", function()
-      local laptopScreen = "Color LCD"
-      local thunderboltDisplay = "Thunderbolt Display"
-      local hpDisplay = "HP Z23i"
-      local windowLayout = {
-         {"Google Chrome",  nil,          thunderboltDisplay, hs.layout.left50,    nil, nil},
-         {"Terminal",    nil,          hpDisplay, hs.layout.maximized,   nil, nil},
-         {"Spotify",    nil,          laptopScreen, hs.layout.maximized,   nil, nil},
-      }
-      hs.layout.apply(windowLayout)
-      hs.alert.show("Layout home loaded")
+  hs.grid.GRIDWIDTH  = 4
+  hs.grid.GRIDHEIGHT = 4
+  hs.grid.MARGINX    = 0
+  hs.grid.MARGINY    = 0
+  hs.alert.show("External Monitor Layout")
 end)
 
-
--- hs.hotkey.bind({"cmd", "shift", "ctrl"}, "M", function()
---       local laptopScreen = "Color LCD"
---       local thunderboltDisplay = "Thunderbolt Display"
---       local hpDisplay = "HP Z23i"
---       local windowLayout = {
---          {"Google Chrome",  nil,          hpdisplay, hs.layout.maximized,    nil, nil},
---          {"Preview",  nil,          thunderboltDisplay, hs.layout.right50,    nil, nil},
---          {"nvALT",    nil,          thunderboltDisplay, geometry.rect(0.5, 0.5, 0.5, 0.5),   nil, nil},
---          {"Mail",    nil,          thunderboltDisplay, geometry.rect(0.5, 0, 0.5, 0.5),   nil, nil},
---          {"Microsoft Outlook",    nil,          thunderboltDisplay, geometry.rect(0.5, 0, 0.5, 0.5),   nil, nil},
---          {"Messages",    nil,          thunderboltDisplay, geometry.rect(0.5, 0, 0.5, 0.5),   nil, nil},
---          {"Terminal",    nil,          thunderboltDisplay, hs.layout.maximized,   nil, nil},
---          {"Spotify",    nil,          laptopScreen, hs.layout.maximized,   nil, nil},
---          {"Safari",    nil,          laptopScreen, hs.layout.maximized,   nil, nil},
---       }
---       hs.layout.apply(windowLayout)
---       hs.alert.show("Layout loaded")
--- end)
-
-
--- hs.hotkey.bind({"cmd", "shift", "ctrl"}, "T", function()
---       local laptopScreen = "Color LCD"
---       local thunderboltDisplay = "Thunderbolt Display"
---       local hpDisplay = "HP Z23i"
---       local windowLayout = {
---          {"Google Chrome",  nil,          laptopScreen, hs.layout.maximized,    nil, nil},
---          {"Terminal",    nil,          hpDisplay, hs.layout.maximized,   nil, nil},
---          {"Spotify",    nil,          laptopScreen, hs.layout.maximized,   nil, nil},
---          {"Preview",  nil,          thunderboltDisplay, hs.layout.right50,    nil, nil},
---          {"Microsoft Outlook",    nil,          thunderboltDisplay, geometry.rect(0.5, 0, 0.5, 0.5),   nil, nil},
---          {"Mail",    nil,          thunderboltDisplay, geometry.rect(0.5, 0, 0.5, 0.5),   nil, nil},
---          {"nvALT",    nil,          thunderboltDisplay, geometry.rect(0.5, 0.5, 0.5, 0.5),   nil, nil},
---       }
---       hs.layout.apply(windowLayout)
---       hs.alert.show("Layout loaded")
--- end)
-
-
--- -- work
--- hs.hotkey.bind({"cmd", "shift", "ctrl"}, "W", function()
---       local laptopScreen = "Color LCD"
---       local Display = "Display"
---       local hpDisplay = "HP LP2065"
---       local windowLayout = {
---          {"Google Chrome",  nil,          hpDisplay, hs.layout.maximized,    nil, nil},
---          {"Emacs",    nil,          Display, geometry.rect(0.5, 0.5, 0.5, 0.5),   nil, nil},
---          {"Preview",  nil,          hpDisplay, hs.layout.maximized,    nil, nil},
---          {"nvALT",    nil,          Display, geometry.rect(0.5, 0.5, 0.5, 0.5),   nil, nil},
---          {"Mail",    nil,          Display, geometry.rect(0.5, 0, 0.5, 0.5),   nil, nil},
---          {"Microsoft Outlook",    nil,          Display, geometry.rect(0.5, 0, 0.5, 0.5),   nil, nil},
---          {"Messages",    nil,          Display, geometry.rect(0.5, 0, 0.5, 0.5),   nil, nil},
---          {"Terminal",    nil,          Display, hs.layout.maximized,   nil, nil},
---          {"Spotify",    nil,          laptopScreen, hs.layout.maximized,   nil, nil},
---          {"Safari",    nil,          laptopScreen, hs.layout.maximized,   nil, nil},
---       }
---       hs.layout.apply(windowLayout)
---       hs.alert.show("Layout loaded")
--- end)
-
-hs.hotkey.bind({"cmd", "shift", "ctrl"}, "P", function()
-      local laptopScreen = "Color LCD"
-      local windowLayout = {
-         {"Chrome",  nil,          laptopScreen, geometry.rect(0, 0.5, 0.5, 0.5),    nil, nil},
-         {"Terminal",    nil,          laptopScreen, geometry.rect(0.0, 0.0, 0.5, 0.5),   nil, nil},
-      }
-      hs.layout.apply(windowLayout)
-      hs.alert.show("Layout loaded")
+hs.hotkey.bind({"cmd", "shift", "ctrl"}, "L", function()
+  hs.grid.GRIDWIDTH  = 3
+  hs.grid.GRIDHEIGHT = 3
+  hs.grid.MARGINX    = 0
+  hs.grid.MARGINY    = 0
+  hs.alert.show("Laptop Monitor Layout")
 end)
 
-------------------
--- wifi watcher --
-------------------
+-- Wifi status watcher
 hs.wifi.watcher.new(function ()
-    local currentWifi = hs.wifi.currentNetwork()
-    -- short-circuit if disconnecting
-    if not currentWifi then return end
-    hs.alert.show("Wi-Fi connected to " .. currentWifi, 3)
-    hs.audiodevice.defaultOutputDevice():setVolume(0)
+  local currentWifi = hs.wifi.currentNetwork()
+  -- short-circuit if disconnecting
+  if not currentWifi then return end
+  hs.alert.show("Wi-Fi connected to " .. currentWifi, 3)
+  hs.audiodevice.defaultOutputDevice():setVolume(0)
 end):start()
