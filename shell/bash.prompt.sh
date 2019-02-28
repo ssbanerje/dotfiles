fbb142c34654f88f9efed76d29aa1e642232d218
Subho Banerjee <ssbanerje@gmail.com>
2019-02-28 17:01:32 -0600

    Fix bash prompt on linux

--- shell/bash.prompt.sh
+++ shell/bash.prompt.sh
@@ -1,5 +1,12 @@
 ## Custom prompt
 # Colors
+
+# Check device type
+case "$TERM" in
+    xterm-color|*-256color) color_prompt=yes;;
+esac
+
+if [ "$color_prompt" = yes ]; then
        RED="\[\033[0;31m\]"
       PINK="\[\033[1;31m\]"
     YELLOW="\[\033[1;33m\]"
@@ -11,6 +18,20 @@
       CYAN="\[\033[1;36m\]"
      BROWN="\[\033[0;33m\]"
 COLOR_NONE="\[\033[0m\]"
+else
+       RED=""
+      PINK=""
+    YELLOW=""
+     GREEN=""
+  LT_GREEN=""
+      BLUE=""
+     WHITE=""
+    PURPLE=""
+      CYAN=""
+     BROWN=""
+COLOR_NONE=""
+fi
+unset color_prompt force_color_prompt
 
 LIGHTNING_BOLT="⚡"
       UP_ARROW="↑"
@@ -23,11 +44,11 @@ LIGHTNING_BOLT="⚡"
 
 
 function parse_git_branch {
-  branch_pattern="^# On branch ([^${IFS}]*)"
-  remote_pattern_ahead="# Your branch is ahead of"
-  remote_pattern_behind="# Your branch is behind"
-  remote_pattern_ff="# Your branch (.*) can be fast-forwarded."
-  diverge_pattern="# Your branch and (.*) have diverged"
+  branch_pattern="^On branch ([^${IFS}]*)"
+  remote_pattern_ahead="Your branch is ahead of"
+  remote_pattern_behind="Your branch is behind"
+  remote_pattern_ff="Your branch (.*) can be fast-forwarded."
+  diverge_pattern="Your branch and (.*) have diverged"
 
   git_status="$(git status 2> /dev/null)"
   if [[ ! ${git_status} =~ ${branch_pattern} ]]; then
@@ -38,10 +59,11 @@ function parse_git_branch {
     [[ -d "$toplevel/.git/rebase-merge" || -d "$toplevel/.git/rebase-apply" ]] && {
       sha_file="$toplevel/.git/rebase-merge/stopped-sha"
       [[ -e "$sha_file" ]] && {
-        sha=`cat "${sha_file}"`
+        sha=$(cat "${sha_file}")
       }
       echo "${PINK}(rebase in progress)${COLOR_NONE} ${sha}"
     }
+
     return
   fi
 
@@ -81,26 +103,25 @@ function parse_git_branch {
     remote="${YELLOW}${UD_ARROW}"
   fi
 
-  echo "${remote}${remote_ff}${GREEN}(${branch})${COLOR_NONE}${git_is_dirty}${COLOR_NONE}"
+  echo " ${remote}${remote_ff}${GREEN}(${branch})${COLOR_NONE}${git_is_dirty}${COLOR_NONE}"
 }
 
-function setWindowTitle {
-  case $TERM in
-    *xterm*|ansi)
-      echo -n -e "\033]0;$*\007"
+# Set title
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+  export debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# Set propmpt
+function set_prompt {
+  export PS1=""
+  case "$TERM" in
+  xterm*|rxvt*)
+     PS1="\[\e]0;\u@\h: \w\a\]"
+      ;;
+  *)
       ;;
   esac
-}
-
-function set_prompt {
-  [[ -n $HOMEBREW_DEBUG_INSTALL ]] && \
-    homebrew_prompt="${BROWN}Homebrew:${COLOR_NONE} debugging ${HOMEBREW_DEBUG_INSTALL}\n"
-
-  git_prompt="$(parse_git_branch)"
-
-  export PS1="[\w] ${git_prompt}${COLOR_NONE}\n${homebrew_prompt}\$ "
-
-  setWindowTitle "${PWD/$HOME/~}"
+  PS1="$PS1${debian_chroot:+($debian_chroot)}[${GREEN}\u${COLOR_NONE}@${BLUE}\h${COLOR_NONE}:${PURPLE}\w${COLOR_NONE}]$(parse_git_branch)${COLOR_NONE}\$ "
 }
 export PROMPT_COMMAND=set_prompt
 
