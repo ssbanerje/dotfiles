b0886486b779dfb791e97fa4a0da884cc02857e2
Subho Banerjee <ssbanerje@gmail.com>
2020-07-01 17:44:08 -0500

    increase key repeat rate for linux

--- shell/common_settings.Linux.sh
+++ shell/common_settings.Linux.sh
@@ -3,4 +3,5 @@ BASE16_SHELL=$HOME/.config/base16-shell/
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
 source ${HOME}/.config/base16-shell/scripts/base16-monokai.sh
 
-
+# Set rate for repeated characters
+xset r rate 300 50
