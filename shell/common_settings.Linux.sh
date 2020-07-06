bbe21b1f63caf065a54177e7b65e70cd761cb2de
Subho Banerjee <ssbanerje@gmail.com>
2020-07-06 03:35:08 -0500

    Changing color combinations

--- shell/common_settings.Linux.sh
+++ shell/common_settings.Linux.sh
@@ -1,5 +1,5 @@
 # Source color schemes
 BASE16_SHELL=$HOME/.config/base16-shell/
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
-source ${HOME}/.config/base16-shell/scripts/base16-monokai.sh
+source ${HOME}/.config/base16-shell/scripts/base16-helios.sh
 
