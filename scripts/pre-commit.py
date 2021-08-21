3af596559d3b716a8b83e2708b46e64458652a72
Subho Banerjee <ssbanerje@gmail.com>
2021-08-21 00:28:22 -0500

    cleaning up workflow for modified profiles

--- scripts/pre-commit.py
+++ scripts/pre-commit.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
-# pylint: disable=line-too-long
 
+import functools
 import os
 import sys
+
 import yaml
-import functools
 
 
 def check_githubactions_yaml_configs(args=None):
-    configs = [
-        f.rsplit('.')[0] for f in os.listdir('meta/config/')
-        if f.endswith('.yaml')
-    ]
+    configs = [f.rsplit('.')[0] for f in os.listdir('meta/config/') if f.endswith('.yaml')]
     with open('.github/workflows/build.yml') as github:
         try:
             yaml_configs = yaml.safe_load(github)
         except yaml.YAMLError as exc:
             print(exc)
     yaml_configs = yaml_configs['jobs']['install-config']['strategy']['matrix']['config']
-    return configs.sort() == yaml_configs.sort()
+    configs.sort()
+    yaml_configs.sort()
+    print(configs ==  yaml_configs)
+    return configs == yaml_configs
 
 
 def main(args=None):
