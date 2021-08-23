20f75ed0be5e8758d72cf342d592255f13c3ecf1
Subho Banerjee <ssbanerje@gmail.com>
2021-08-23 11:23:20 -0500

    check for profiles in the pre-commit hook

--- scripts/pre-commit.py
+++ scripts/pre-commit.py
@@ -7,21 +7,42 @@ import yaml
 
 
 def check_githubactions_yaml_configs(args=None):
-    configs = [f.rsplit('.')[0] for f in os.listdir('meta/config/') if f.endswith('.yaml')]
+    configs = [
+        f.rsplit('.')[0] for f in os.listdir('meta/config/')
+        if f.endswith('.yaml')
+    ]
     with open('.github/workflows/build.yml') as github:
         try:
             yaml_configs = yaml.safe_load(github)
         except yaml.YAMLError as exc:
             print(exc)
-    yaml_configs = yaml_configs['jobs']['test-config']['strategy']['matrix']['config']
+    yaml_configs = yaml_configs['jobs']['test-config']['strategy']['matrix'][
+        'config']
     configs.sort()
     yaml_configs.sort()
     print(configs == yaml_configs)
     return configs == yaml_configs
 
 
+def check_githubactions_yaml_profiles(args=None):
+    profiles = [f.rsplit('.')[0] for f in os.listdir('meta/profiles/')]
+    with open('.github/workflows/build.yml') as github:
+        try:
+            yaml_configs = yaml.safe_load(github)
+        except yaml.YAMLError as exc:
+            print(exc)
+    yaml_profiles = yaml_configs['jobs']['test-profiles']['strategy'][
+        'matrix']['profile']
+    profiles.sort()
+    yaml_profiles.sort()
+    print(profiles == yaml_profiles)
+    return profiles == yaml_profiles
+
+
 def main(args=None):
-    funcs = [check_githubactions_yaml_configs]
+    funcs = [
+        check_githubactions_yaml_configs, check_githubactions_yaml_profiles
+    ]
     return functools.reduce(lambda a, f: a and f(args), funcs, True)
 
 
