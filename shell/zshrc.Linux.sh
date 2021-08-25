df52e5a891b743df2655f4c5a0d81d320250ee05
Subho Banerjee <ssbanerje@gmail.com>
2021-08-25 13:32:52 -0500

    temporarily disable grc in linux

--- shell/zshrc.Linux.sh
+++ shell/zshrc.Linux.sh
@@ -1,6 +1,6 @@
 # Enable grc on all commands
 [[ -s "/etc/grc.zsh" ]] && source /etc/grc.zsh
-type ls &> /dev/null && unset -f ls # Do not want this from grc
+#type ls &> /dev/null && unset -f ls # Do not want this from grc
 
 # Enable fzf
 [[ -s "/usr/share/doc/fzf/examples/completion.zsh" ]] && \
