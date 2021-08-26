e43f1e7c41898c8f202f3edd21d24c6ddc39e217
Subho Banerjee <ssbanerje@gmail.com>
2021-08-25 23:07:23 -0500

    change order on python path

--- shell/env.Darwin.sh
+++ shell/env.Darwin.sh
@@ -8,9 +8,7 @@ fi
 if [[ ! "$PATH" == *"/usr/local/sbin"* ]]; then
   export PATH="/usr/local/sbin:$PATH"
 fi
-if [[ ! "$PATH" == *"/usr/local/opt/python/libexec/bin"* ]]; then
-  export PATH="/usr/local/opt/python/libexec/bin:$PATH" #< TODO Remove when Python3 is default
-fi
+export PATH="/usr/local/opt/python/libexec/bin:$PATH" #< TODO Remove when Python3 is default
 
 # Use LLVM tools from homebrew vs Apple
 if [[ ! "$PATH" == *"/usr/local/opt/llvm/bin"* ]]; then
