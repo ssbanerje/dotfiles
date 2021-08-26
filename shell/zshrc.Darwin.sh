c0b96dc09903e44987883d9b088097c12d6ab92d
Subho Banerjee <ssbanerje@gmail.com>
2021-08-26 11:01:23 -0500

    move to zsh-vi-mode

--- shell/zshrc.Darwin.sh
+++ shell/zshrc.Darwin.sh
@@ -4,8 +4,11 @@ if [[ -a $HOME/.config/iterm2_shell_integration.zsh ]]; then
 fi
 
 # Setup fzf
-[[ $- == *i* ]] && source "$(brew --prefix)/opt/fzf/shell/completion.zsh" 2> /dev/null
-source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
+function fzf_init() {
+  [[ $- == *i* ]] && source "$(brew --prefix)/opt/fzf/shell/completion.zsh" 2> /dev/null
+  source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
+}
+zvm_after_init_commands+=(fzf_init)
 
 # Enable grc on all commands
 [[ -s "$(brew --prefix)/etc/grc.zsh" ]] && source "$(brew --prefix)/etc/grc.zsh"
--- shell/zshrc.Linux.sh
+++ shell/zshrc.Linux.sh
@@ -3,7 +3,10 @@
 #type ls &> /dev/null && unset -f ls # Do not want this from grc
 
 # Enable fzf
-[[ -s "/usr/share/doc/fzf/examples/completion.zsh" ]] && \
-  source /usr/share/doc/fzf/examples/completion.zsh
-[[ -s "/usr/share/doc/fzf/examples/key-bindings.zsh" ]] && \
-  source /usr/share/doc/fzf/examples/key-bindings.zsh
+function fzf_init() {
+  [[ -s "/usr/share/doc/fzf/examples/completion.zsh" ]] && \
+    source /usr/share/doc/fzf/examples/completion.zsh
+  [[ -s "/usr/share/doc/fzf/examples/key-bindings.zsh" ]] && \
+    source /usr/share/doc/fzf/examples/key-bindings.zsh
+}
+zvm_after_init_commands+=(fzf_init)
