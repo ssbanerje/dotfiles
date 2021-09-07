24b207d96b7f0dddc15a4d44d6f2a966b4d9ce50
Subho Banerjee <ssbanerje@gmail.com>
2021-09-06 20:30:30 -0500

    fix fzf setting after zsh-defer

--- shell/zshrc.Darwin.sh
+++ shell/zshrc.Darwin.sh
@@ -9,11 +9,8 @@ if [[ "$TERM_PROGRAM" == "iTerm.app" && -f "$HOME/.config/iterm2_shell_integrati
 fi
 
 # Setup fzf
-function fzf_init() {
-  [[ $- == *i* ]] && source "$(brew --prefix)/opt/fzf/shell/completion.zsh"
-  source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
-}
-zvm_after_init_commands+=(fzf_init)
+[[ $- == *i* ]] && source "$(brew --prefix)/opt/fzf/shell/completion.zsh"
+source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
 
 # Enable grc on all commands
 [[ -f "$(brew --prefix)/etc/grc.zsh" ]] && source "$(brew --prefix)/etc/grc.zsh"
--- shell/zshrc.Linux.sh
+++ shell/zshrc.Linux.sh
@@ -2,10 +2,9 @@
 [[ -s "/etc/grc.zsh" ]] && source /etc/grc.zsh
 
 # Enable fzf
-function fzf_init() {
-  [[ -s "/usr/share/doc/fzf/examples/completion.zsh" ]] && \
-    source /usr/share/doc/fzf/examples/completion.zsh
-  [[ -s "/usr/share/doc/fzf/examples/key-bindings.zsh" ]] && \
-    source /usr/share/doc/fzf/examples/key-bindings.zsh
-}
-zvm_after_init_commands+=(fzf_init)
+if [[ -s "/usr/share/doc/fzf/examples/completion.zsh" ]]; then
+  source /usr/share/doc/fzf/examples/completion.zsh
+fi
+if [[ -s "/usr/share/doc/fzf/examples/key-bindings.zsh" ]]; then
+  source /usr/share/doc/fzf/examples/key-bindings.zsh
+fi
