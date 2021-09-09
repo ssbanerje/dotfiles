aef016d6f14156ad387ce4ec2207aafd2c5408ec
Subho Banerjee <ssbanerje@gmail.com>
2021-09-09 13:59:22 -0500

    fixes shim and git bugs

--- scripts/shim.sh
+++ scripts/shim.sh
@@ -10,29 +10,32 @@ else
 fi
 
 # Get the ssh-agent
-if [[ ! -n "${SSH_AUTH_SOCK}" ]]; then
+if [[ ! -n "$SSH_AUTH_SOCK" ]]; then
   STARTED_SSH_AGENT=1
   eval "$(ssh-agent)" > /dev/null
   ssh-add
 fi
 
+# Get GPG socket
+export GPG_EXTRA_SOCK="$(gpgconf --list-dirs agent-extra-socket)"
+gpgconf --launch gpg-agent
+
 # Run docker
 docker pull "$IMAGE"
-
 docker run -it --rm \
   --privileged \
   -u "$(id -u):$(id -g)" \
   --network host \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -v "/run/user/$(id -u)/:/run/user/$(id -u)/:ro" \
-  -v "${SSH_AUTH_SOCK}":/ssh-auth-socket \
+  -v "$SSH_AUTH_SOCK":/ssh-auth-socket \
   -e SSH_AUTH_SOCK=/ssh-auth-socket \
-  -e TERM="$TERM" \
-  -v "${PWD}:/cwd/:rw" \
+  -v "$GPG_EXTRA_SOCK":/gpg-agent-extra-socket \
+  -e GPG_EXTRA_SOCK="$GPG_EXTRA_SOCK" \
+  -v "$PWD:/cwd/:rw" \
   -w "/cwd" \
   "$IMAGE" "$@"
 
 # Cleanup
-if [[ -n "${STARTED_SSH_AGENT}" ]]; then
+if [[ -n "$STARTED_SSH_AGENT" ]]; then
   eval $(ssh-agent -k) > /dev/null
 fi
